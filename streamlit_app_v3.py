3ff0fffda8fec53aed7d5de41e12f217af01a079
Viet Pham <127240050+viettpp@users.noreply.github.com>
2023-10-04 17:56:46 +0200

    Merging test branch to master branch (#1)
    * Add files via upload
    
    Removed sidebar, set standard temperature, llama2 to 13B, and stored API in secret.
    
    * Delete streamlit_app_v2.py
    
    * Delete streamlit_app.py
    
    * Delete app_v1.py
    
    * Update streamlit_app_v3.py
    
    Updated chat_input
    
    * Update config.toml
    
    Changed theme to dark mode
    
    * Update README.md
    
    * Delete Llama2.ipynb
    
    * Delete llama2-local.py

--- streamlit_app_v3.py
+++ streamlit_app_v3.py
@@ -3,38 +3,26 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="🤖🧠💬 VietBot")
 
 # Replicate Credentials
-with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
+replicate_api = st.secrets['REPLICATE_API_TOKEN']
+
+# Set hyperparameters for LLM
+temperature = 0.5
+top_p = 0.7
+max_length = 120
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Hola! Ever thought Viet had a twin? Well, you're chatting with him. How may I assist you today?"}]
 
-# Display or clear chat messages
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
-# Function for generating LLaMA2 response
-# Refactored from https://github.com/a16z-infra/llama2-chatbot
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
@@ -44,11 +32,11 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
     return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+if prompt := st.chat_input("Ask me anything"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
@@ -56,7 +44,7 @@ if prompt := st.chat_input(disabled=not replicate_api):
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
+        with st.spinner("Contacting Viet to confirm the correct answer..."):
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
