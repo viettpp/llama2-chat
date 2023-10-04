efc1f5b8f971107e99a0a054d9bbc35ffb8756ff
Viet Pham <127240050+viettpp@users.noreply.github.com>
2023-10-04 17:06:27 +0200

    Add files via upload
    Removed sidebar, set standard temperature, llama2 to 13B, and stored API in secret.

---
+++ streamlit_app_v3.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import replicate
+import os
+
+# App title
+st.set_page_config(page_title="🤖🧠💬 VietBot")
+
+# Replicate Credentials
+replicate_api = st.secrets['REPLICATE_API_TOKEN']
+
+# Set hyperparameters for LLM
+temperature = 0.5
+top_p = 0.7
+max_length = 120
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hola! Ever thought Viet had a twin? Well, you're chatting with him. How may I assist you today?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Contacting Viet to confirm the correct answer..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
